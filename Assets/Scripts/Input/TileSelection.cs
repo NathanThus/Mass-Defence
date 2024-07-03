c9d1e6b3b2bcf2828b15c797c83fa87983623813
NathanThus <nathan.thus@outlook.com>
2024-07-03 12:48:05 +0200

    🚧 Create basic classes for tower selection

---
+++ Assets/Scripts/Input/TileSelection.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace NathanThus.MassDefence.Input
+{
+    public class TileSelection : MonoBehaviour
+    {
+        private InputAction leftMouseAction;
+        private InputAction rightMouseAction;
+
+        private void OnEnable()
+        {
+
+        }
+
+        private void OnDisable()
+        {
+
+        }
+    }
+}
