6b678bab1ee132d98f750c2d1ddebb098f59e231
NathanThus <nathan.thus@outlook.com>
2024-07-03 14:25:36 +0200

    ✨ Add Ability to target game board

--- Assets/Scripts/Input/TileSelection.cs
+++ Assets/Scripts/Input/TileSelection.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,17 +8,62 @@ namespace NathanThus.MassDefence.Input
 {
     public class TileSelection : MonoBehaviour
     {
-        private InputAction leftMouseAction;
-        private InputAction rightMouseAction;
+        #region Serialized Fields
+
+        [SerializeField] private Camera _camera;
+
+        #endregion
+
+        #region Fields
+        private MassDefenceInputActions _massDefenceInputActions;
+        private InputAction _leftMouseAction;
+        private InputAction _rightMouseAction;
+
+        private const float MaxDistance = 100f;
+        #endregion
+
+        #region Setup
+        private void Awake()
+        {
+            _massDefenceInputActions = new();
+            _leftMouseAction = _massDefenceInputActions.TowerSelection.LeftMouse;
+            _rightMouseAction = _massDefenceInputActions.TowerSelection.RightMouse;
+        }
 
         private void OnEnable()
         {
-
+            _leftMouseAction.performed += HandleLeftMouseClick;
+            _rightMouseAction.performed += HandleRightMouseClick;
+            _massDefenceInputActions.Enable();
         }
 
         private void OnDisable()
         {
-
+            _leftMouseAction.performed -= HandleLeftMouseClick;
+            _rightMouseAction.performed -= HandleRightMouseClick;
+            _massDefenceInputActions.Disable();
         }
+
+        #endregion
+
+        #region Private
+        private void HandleRightMouseClick(InputAction.CallbackContext _)
+        {
+        }
+
+        private void HandleLeftMouseClick(InputAction.CallbackContext _)
+        {
+            var mousePos = Mouse.current.position.ReadValue();
+            if (!Physics.Raycast(_camera.ScreenPointToRay(new Vector3(mousePos.x, mousePos.y)),
+                                out RaycastHit hit,
+                                MaxDistance))
+            {
+                return;
+            }
+
+            Destroy(hit.collider.gameObject);
+        }
+
+        #endregion
     }
 }
