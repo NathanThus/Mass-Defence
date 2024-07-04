e1959960acf165cdc0ce3dcbe2d85f0881bd9c77
NathanThus <nathan.thus@outlook.com>
2024-07-04 15:23:49 +0200

    ✨ Improve Map Generation
    Goal and Start cannot be adjacent, allowing more flow through the map.

--- Assets/Scripts/MapGeneration/MapGenerator.cs
+++ Assets/Scripts/MapGeneration/MapGenerator.cs
@@ -9,40 +9,70 @@ namespace NathanThus.MassDefence.MapGeneration
         #region Serialized Fields
         [Header("Settings")]
         [SerializeField] private Vector2 _size;
+        [SerializeField] private int _spacing;
+        [SerializeField] private List<Transform> _tiles;
+
 
         [Header("Ground Plates")]
-        [SerializeField] private Transform _platePrefab;
-        [SerializeField] private int _spacing;
-        [SerializeField] private List<Transform> _plates;
+        [SerializeField] private Transform _basicTile;
+        [SerializeField] private Transform _startPrefab;
+        [SerializeField] private Transform _goalPrefab;
+        [SerializeField] private Transform _plateHolder;
 
         #endregion
 
         #region Public
 
-        public void UpdatePlates()
+        public void UpdateTiles()
         {
-            ClearPlates();
+            bool isValidLocationPair = false;
+            Vector2 startPos = Vector2.zero, endPos = Vector2.zero;
+            while (!isValidLocationPair)
+            {
+                startPos = new((int)Random.Range(-_size.x / 2, _size.x / 2), (int)Random.Range(-_size.y / 2, _size.y / 2));
+                endPos = new((int)Random.Range(-_size.x / 2, _size.x / 2), (int)Random.Range(-_size.y / 2, _size.y / 2));
+
+                if (Vector2.Distance(startPos, endPos) > _spacing) isValidLocationPair = true;
+            }
+
+            ClearTiles();
 
             for (int x = (int)-_size.x / 2; x < _size.x / 2; x++)
             {
                 for (int y = (int)-_size.y / 2; y < _size.y / 2; y++)
                 {
-                    var plate = Instantiate(_platePrefab,
-                                            new Vector3(x * _spacing, 0, y * _spacing),
-                                            Quaternion.identity,
-                                            this.transform);
-                    _plates.Add(plate);
+                    if (x == startPos.x && y == startPos.y)
+                    {
+                        CreateTile(_startPrefab, x, y);
+                    }
+                    else if (x == endPos.x && y == endPos.y)
+                    {
+                        CreateTile(_goalPrefab, x, y);
+                    }
+                    else
+                    {
+                        CreateTile(_basicTile, x, y);
+                    }
                 }
+            }
+
+            void CreateTile(Transform tile, int x, int y)
+            {
+                var plate = Instantiate(tile,
+                                        new Vector3(x * _spacing, 0, y * _spacing),
+                                        Quaternion.identity,
+                                        _plateHolder);
+                _tiles.Add(plate);
             }
         }
 
-        public void ClearPlates()
+        public void ClearTiles()
         {
-            foreach (var item in _plates)
+            foreach (var item in _tiles)
             {
                 DestroyImmediate(item.gameObject);
             }
-            _plates.Clear();
+            _tiles.Clear();
         }
 
         #endregion
