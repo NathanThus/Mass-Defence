56b21a158143c32594eb42a8ffc2c44c59cac694
NathanThus <nathan.thus@outlook.com>
2024-07-05 16:53:01 +0200

    ✨ Create Enemy Authoring

---
+++ Assets/Scripts/Enemy/EnemyAuthoring.cs
@@ -0,0 +1,22 @@
+using Unity.Entities;
+using UnityEngine;
+
+namespace NathanThus.MassDefence.Enemy
+{
+    public class EnemyAuthoring : MonoBehaviour
+    {
+
+        class Baker : Baker<EnemyAuthoring>
+        {
+            public override void Bake(EnemyAuthoring authoring)
+            {
+                var entity = GetEntity(authoring, TransformUsageFlags.Dynamic);
+                AddComponent(entity, new Enemy{});
+            }
+        }
+    }
+
+    public struct Enemy : IComponentData
+    {
+    }
+}
